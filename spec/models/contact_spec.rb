f745cbfb06fbc2b45e59346294a026f1b4099e0c
Drew Williams <drewwilliams5280@gmail.com>
2021-01-08 13:29:50 -0700

    Adds contact creation test

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Contact, type: :model do
     it {should validate_presence_of :carrier}
     it {should validate_presence_of :line_type}
   end
+
+  describe "creation" do 
+    it "can create a new contact" do 
+      expect(Contact.count).to eq(0)
+      contact = Contact.create!(phone_number: "+8175010256", carrier: "AT&T", line_type: "Wireless")
+      expect(Contact.count).to eq(1)
+      expect(contact.phone_number).to eq("+8175010256")
+      expect(contact.carrier).to eq("AT&T")
+      expect(contact.line_type).to eq("Wireless")
+    end
+  end
 end
