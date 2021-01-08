07ab1d2ac0764c4f79a39a7e92eed695645215a7
Drew Williams <drewwilliams5280@gmail.com>
2021-01-08 13:14:32 -0700

    Initial contact model tests setup

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do 
+    it { should have_many :contact_messages }
+    it { should have_many :telnyx_messages }
+  end
+
+  describe "validations" do
+    it {should validate_presence_of :phone_number}
+    it {should validate_presence_of :carrier}
+    it {should validate_presence_of :line_type}
+  end
 end
