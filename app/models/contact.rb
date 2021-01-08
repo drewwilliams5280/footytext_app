fb0b1e97e42f51517250d9e5084223c8e337bd32
Drew Williams <drewwilliams5280@gmail.com>
2021-01-08 13:23:31 -0700

    Model relationships and validations setup and tested

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
 class Contact < ApplicationRecord
+  validates_presence_of :phone_number, require: true
+  validates_presence_of :carrier, require: true
+  validates_presence_of :line_type, require: true
+
+  has_many :contact_messages
+  has_many :telnyx_messages
 end
+
+#   validates_presence_of :latitude, require: true
+#   validates_presence_of :longitude, require: true
+#   belongs_to :user
+#   has_many :likes, dependent: :destroy
+#   has_many :likers, :through => :likes, :source => :user
--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,3 +1,6 @@
 class ContactMessage < ApplicationRecord
+  validates_presence_of :text, require: true
+  validates_presence_of :received_at, require: true
+
   belongs_to :contact
 end
