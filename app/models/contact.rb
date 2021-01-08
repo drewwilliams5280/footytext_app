ddb1449f98768c0c7156814a8d397157f7b9e83d
Drew Williams <drewwilliams5280@gmail.com>
2021-01-08 13:24:02 -0700

    Edits contact.rb to remove commented out code

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,9 +6,3 @@ class Contact < ApplicationRecord
   has_many :contact_messages
   has_many :telnyx_messages
 end
-
-#   validates_presence_of :latitude, require: true
-#   validates_presence_of :longitude, require: true
-#   belongs_to :user
-#   has_many :likes, dependent: :destroy
-#   has_many :likers, :through => :likes, :source => :user
