adc2ed297f111e0d780059916cfbffb267afbd1c
Drew Williams <drewwilliams5280@gmail.com>
2021-01-08 22:14:34 -0700

    Adds guard for outbound messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,13 @@
 class MessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
   def create
-    require 'pry'; binding.pry
     json = JSON.parse(request.body.read)
-    contact = Contact.find_or_create_by(json['data']['payload']['from'])
-    ContactMessage.create(contact: contact, 
-                          text: json['data']['payload']['text'], 
-                          received_at: DateTime.parse(json['data']['payload']['received_at'])
-                          )
+    unless json['data']['direction'] == "outbound"
+      contact = Contact.find_or_create_by(json['data']['payload']['from'])
+      ContactMessage.create(contact: contact, 
+                            text: json['data']['payload']['text'], 
+                            received_at: DateTime.parse(json['data']['payload']['received_at'])
+                            )
+    end
   end
 end
