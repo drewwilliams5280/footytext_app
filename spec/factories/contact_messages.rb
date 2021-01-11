90e22de8de1b80acfc5577171528e8ed17c5aa96
Drew Williams <drewwilliams5280@gmail.com>
2021-01-11 16:22:40 -0700

    Removes test data that is unneccesary after recent changes

--- spec/factories/contact_messages.rb
+++ spec/factories/contact_messages.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :contact_message do
     text { "MyString" }
-    received_at { "2021-01-08 12:50:00" }
     contact { nil }
   end
 end
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :contact do
     phone_number { "MyString" }
-    carrier { "AT&T" }
-    line_type { "Wireless" }
   end
 end
