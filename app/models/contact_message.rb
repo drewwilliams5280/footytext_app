aae77cc17f08d0425b39e9166a9e389cbebccb94
Drew Williams <drewwilliams5280@gmail.com>
2021-02-05 09:38:13 -0700

    Init commit

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,19 +1,33 @@
+require 'footy_text'
 class ContactMessage < ApplicationRecord
+  include FootyText
   validates_presence_of :text, require: true
   belongs_to :contact
 
   after_create :respond
   def respond
     if pizza? && ice_cream?
-      TelnyxMessage.create!(contact: contact, text: "Chicago pizza is the best and I prefer gelato")
+      puts "Pizza and ice cream"
     elsif pizza?
-      TelnyxMessage.create!(contact: contact, text: "Chicago pizza is the best")
+      puts "Pizza"
     elsif ice_cream?
-      TelnyxMessage.create!(contact: contact, text: "I prefer gelato")
+      puts "ice cream"
     else
-      TelnyxMessage.create!(contact: contact, text: "Please send either the word ‘pizza’ or ‘ice cream’ for a different response")
+      puts "Please send either the word ‘pizza’ or ‘ice cream’ for a different response"
     end
   end
+
+  # def respond
+  #   if pizza? && ice_cream?
+  #     TelnyxMessage.create!(contact: contact, text: "Chicago pizza is the best and I prefer gelato")
+  #   elsif pizza?
+  #     TelnyxMessage.create!(contact: contact, text: "Chicago pizza is the best")
+  #   elsif ice_cream?
+  #     TelnyxMessage.create!(contact: contact, text: "I prefer gelato")
+  #   else
+  #     TelnyxMessage.create!(contact: contact, text: "Please send either the word ‘pizza’ or ‘ice cream’ for a different response")
+  #   end
+  # end
 
   def pizza?
     text.downcase.include?("pizza")
---
+++ lib/footy_text.rb
@@ -0,0 +1,3 @@
+module FootyText
+
+end
